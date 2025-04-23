915c27a8754a569127ded7cac4bb2658cdff55b8
4rjunc <arjunmundamani@gmail.com>
2025-04-23 12:53:56 +0530

    feat: add imports
    > Add USDC for base mainnet/testnet

---
+++ contracts/Presale.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+//import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+contract Presale is ReentrancyGuard, Pausable {
+  using SafeERC20 for IERC20;
+
+  address private immutable USDC = block.chainid == 8453 ? 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913 : 0x036CbD53842c5426634e7929541eC2318f3dCF7e; // USDC mainnet : testnet
+
+  /// @dev MULTISIGN WALLET ADDRESS 
+  address private immutable MULTISIG_WALLET_ADDRESS = 0x0000000000000000000000000000000000000000; // TODO: Change this 
+  
+  /// @dev owner address 
+  address private _owner;
+
+}
